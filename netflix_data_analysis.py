5f7eee154fe7636faf57e26321559d384137e1dd
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-05-14 22:40:40 +0200

    clean up

--- netflix_data_analysis.py
+++ netflix_data_analysis.py
@@ -47,7 +47,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write('')
 with col2:
-    st.image(netflix_logo)
+    st.image('./' + netflix_logo)
 with col3:
     st.write('')
 st.markdown('#')
